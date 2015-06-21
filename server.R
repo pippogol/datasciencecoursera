1366f11022b992e37c6e82a18cccc0eb86f568f5
pippogol <ryzeng@hotmail.com>
2015-06-21 11:08:21 -0400

    second commit

--- server.R
+++ server.R
@@ -6,19 +6,8 @@ CART1 = rpart(voting ~ civicduty + hawthorne + self + neighbors, data=gerber, cp
 
 shinyServer(
       function(input, output) {
-            
-            #userinput=reactive({userinput=c(input$c,input$h,input$s,input$n)})
-            
-            
-            #userinput=as.factor(userinput)
-            #userinput=as.numeric(userinput)-1
-            #output$v=renderPrint(as.integer(input$c)+as.integer(input$s))
-            
-            
+
             output$v = renderPrint(unname(predict(CART1,newdata=data.frame(civicduty=as.integer(input$c),hawthorne=as.integer(input$h),self=as.integer(input$s),neighbors=as.integer(input$n)))))
-            
-            
-                                       
-            
+        
       }
 )
